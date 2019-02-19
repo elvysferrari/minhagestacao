9534003b39db74286f1d3d085a0e156c815f0416
Elvys <elvys.ferrari@gmail.com>
2019-02-19 10:49:20 -0300

    removido timout

--- src/app/pages/lista-documentos/lista-documentos.page.ts
+++ src/app/pages/lista-documentos/lista-documentos.page.ts
@@ -6,7 +6,7 @@ import { AdMobFree, AdMobFreeBannerConfig, AdMobFreeInterstitialConfig } from '@
 const intersticalConfig: AdMobFreeInterstitialConfig = {
   // add your config here
   // for the sake of this example we will just use the test config
- /*  id: 'ca-app-pub-4358080998486510/8666011597', */
+  /*  id: 'ca-app-pub-4358080998486510/8666011597', */
   isTesting: true,
   autoShow: false
 };
@@ -19,9 +19,9 @@ const intersticalConfig: AdMobFreeInterstitialConfig = {
 export class ListaDocumentosPage implements OnInit {
   documentos: DocumentoModelo[];
   documentosFiltered: DocumentoModelo[] = [];
-  constructor(private docsService: DocumentosService,private admobFree: AdMobFree) {
-    
-   }
+  constructor(private docsService: DocumentosService, private admobFree: AdMobFree) {
+
+  }
 
   ngOnInit() {
     this.documentos = this.docsService.getDocumentList();
@@ -34,36 +34,29 @@ export class ListaDocumentosPage implements OnInit {
     this.documentosFiltered.sort((a: any, b: any) => {
       return a.titulo < b.titulo ? -1 : 1;
     });
-    setTimeout(() =>{
-      this.admobFree.interstitial.config(intersticalConfig);
 
+    let time = new Date().getMinutes() - this.docsService.getBannerTime();
+    if (time >= 2) {
+      this.admobFree.interstitial.config(intersticalConfig);
       this.admobFree.interstitial.prepare()
         .then(() => {
-          let time = new Date().getMinutes() - this.docsService.getBannerTime();
-          alert(time)
-          if(time >= 5){
-            this.admobFree.interstitial.show();
-            this.docsService.setBannerTime();
-            
-          }
-         // this.admobFree.interstitial.show();
+          this.admobFree.interstitial.show();
+          this.docsService.setBannerTime();
         })
         .catch(e => alert(e));
-    }, 1000)
+    }
+
   }
 
-  changeSearch(evt){
-    alert(this.docsService.getBannerTime())
-    alert(new Date().getMinutes())
-    console.log('changeSearch', evt);    
-    if(evt.detail.value == ""){
+  changeSearch(evt) {
+    if (evt.detail.value == "") {
       this.documentosFiltered = this.documentos;
-    }else{
+    } else {
       this.documentosFiltered = this.documentos.filter(x => x.titulo.toLocaleLowerCase().includes(evt.detail.value.toLocaleLowerCase()));
     }
   }
 
-  cancelSearch(evt){
+  cancelSearch(evt) {
     this.documentosFiltered = this.documentos;
   }
 }
